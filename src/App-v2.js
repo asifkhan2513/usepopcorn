b24057df06c8cc6e2c107783f91bc24264774cd0
Mo Asif <141583278+asifkhan2513@users.noreply.github.com>
2025-09-09 21:28:04 +0530

    added by asif khan

--- src/App-v2.js
+++ src/App-v2.js
@@ -68,7 +68,7 @@ function Logo() {
   return (
     <div className="logo">
       <span role="img">🍿</span>
-      <h1>usePopcorn by asif </h1>
+      <h1>usePopcorn by asif khan </h1>
       <p>Created by asif khan </p>
     </div>
   );
